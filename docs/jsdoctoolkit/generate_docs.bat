03b516915714fa8137bdf91782f011b8b50d9ba7
mwheeler <devnull@localhost>
2011-01-31 12:00:03 -0800

    Adding existing framework

---
+++ docs/jsdoctoolkit/generate_docs.bat
@@ -0,0 +1,1 @@
+java -jar jsrun.jar jsdoc-toolkit/app/run.js -a -t=jsdoc-toolkit/templates/jsdoc modules/ -d=module_docs
---
+++ generate_docs.bat
@@ -0,0 +1,1 @@
+java -jar docs/jsdoctoolkit/jsrun.jar docs/jsdoctoolkit/app/run.js -a -t=docs/jsdoctoolkit/templates/jsdoc  -d=docs/ -r modules/
