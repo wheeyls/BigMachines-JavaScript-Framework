f859e77aa1d28186919ad2e359ddfca22ef65ebb
mwheeler <devnull@localhost>
2011-02-01 20:24:51 -0800

    Refactoring starter kit following canon setup.

---
+++ BigMachines Javascript Starter Kit/javascript/commerce_line.js
@@ -0,0 +1,13 @@
+/**
+ * @param dependencies {Array} name of modules this code depends on. Can exclude ".js"
+ * @param callback {Function} function containing this module's functionality.
+ */
+require([], function() {
+  /*
+   * Put all functions for commerce line items here
+   */
+
+  //this function runs when the page loads
+  require.ready(function() {
+  });
+});
