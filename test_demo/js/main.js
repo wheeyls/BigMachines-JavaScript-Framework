ffbdfdcb663c7174ec37e7f85e8af82c082e16c1
tbrandt <devnull@localhost>
2011-02-03 10:19:49 -0800

    standalone testing main

--- test_demo/js/main.js
+++ test_demo/js/main.js
@@ -5,7 +5,7 @@
  * @version 1.0
  */
 
-require(["logger","test_mod","test_mod2","commerce_ids"],function(logger,test_mod,tm2) {
+require(["logger","test_mod","test_mod2","return_to_quote_button","jquery.cookie"],function(logger,test_mod,tm2,rtq) {
 	require.ready(function() {
 		//This function is called when the page is loaded (the DOMContentLoaded
 		//event) and when all required scripts are loaded.
@@ -14,6 +14,8 @@ require(["logger","test_mod","test_mod2","commerce_ids"],function(logger,test_mo
 		//after page load.
 		
 		//logger.setLogLevel("warn");
+		
+		jQuery.cookie("last_transaction","blah");
 		
 		//require(["test_mod"],function(test_mod) {
 			// do some stuff with it
