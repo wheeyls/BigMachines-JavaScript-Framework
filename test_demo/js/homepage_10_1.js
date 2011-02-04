267dd58d0a7340d1156ca1d86e3365625ecd0630
tbrandt <devnull@localhost>
2011-02-04 14:25:07 -0800

    versions of homepage.js for diff bigmachines version (based on jquery availability)

--- test_demo/js/homepage_10_1.js
+++ test_demo/js/homepage_10_1.js
@@ -1,14 +1,14 @@
 /**
- * Version of homepage for 11.0, non-quickstart 11.0 sites
+ * Version of homepage for 10.1 sites
  * @param dependencies {Array} name of modules this code depends on. Can exclude ".js"
  * @param callback {Function} function containing this module's functionality.
  */
-require(["return_to_quote_button"],function(rtq) {
+require(["return_to_quote_button","jquery-1.5"],function(rtq) {
 	/*
 	* Put all functions for homepage here
 	*/
 	//this function runs when the page loads
 	require.ready(function() {
-		rtq.add_button_to_homepage();
+		rtq.add_button_to_homepage(rtq.v10_homepage_callback);
 	});
 });
