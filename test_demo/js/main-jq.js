d073b2d18596d8fb9d783850377def8c860c25da
tbrandt <devnull@localhost>
2011-02-04 09:30:03 -0800

    ability to set jquery version

--- test_demo/js/main-jq.js
+++ test_demo/js/main-jq.js
@@ -1,5 +1,7 @@
 require(["with-jquery"],function(with_jquery) {
 	require.ready(function() {
+		with_jquery.set_jquery_version("jquery-1.4.4.min");
+	
 		with_jquery.with_jquery(function() {
 			jQuery("#jq_div").append("hi from jq-main! Version is: " + jQuery.fn.jquery + "<br/>");
 			require(["jq-inner"],function(jq_inner) {
--- test_demo/js/with-jquery.js
+++ test_demo/js/with-jquery.js
@@ -1,11 +1,17 @@
 define([],function() {
 	var with_jquery = {};
 	
+	var jquery_script = "jquery-1.5";
+	
+	with_jquery.set_jquery_version = function(version) {
+		jquery_script = version;
+	}
+	
 	with_jquery.with_jquery = function(fn) {
 		if(typeof jQuery === "function") {
 			require([],fn);
 		} else {
-			require(["jquery-1.5"],function() {
+			require([jquery_script],function() {
 				$.noConflict();
 				fn();
 			});
