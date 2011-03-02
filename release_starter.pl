49bb4aebf348b13792a4700d5fb90cccca3b1c3c
tbrandt <devnull@localhost>
2011-03-01 18:35:43 -0800

    release_starter now has sensible defaults and uses the blank_template.js for "entry point" files

--- release_starter.pl
+++ release_starter.pl
@@ -2,30 +2,52 @@ use strict;
 use IO::Compress::Zip qw(zip $ZipError);
 use Time::localtime;
 
-if(scalar @ARGV ne 2) {
+my $opt_h;
+if($opt_h) {
 	print "Usage: perl $0 {source} {dest}\n";
 	print 'Copy Javascript files from {source} to {dest}.' . "\n";
 	print 'Also creates a zip archive and sets @version to current time';
 	exit 0;
 }
 
-my $src = @ARGV[0];
-my $dest = @ARGV[1];
+my $src = @ARGV[0] || "test_demo/js";
+my $dest = @ARGV[1] || "js_starter/javascript";
 
-my @list = qw(allplugins-require commerce commerce_line config homepage logger manager sitewide test_util);
+my @list = qw(allplugins-require logger manager test_util);
+my @from_template = qw(commerce commerce_line config homepage sitewide);
+my @all_files = @list;
+
+# read in template
+push(@all_files,@from_template);
+my $template_file = "$src/blank_template.js";
+print "$template_file\n";
+open(TEMPLATE,"<$template_file") or die "No template: $!";
+my @template_lines = ();
+while(<TEMPLATE>) {
+	push(@template_lines,$_);
+}
+close(TEMPLATE);
 
 my @date = localtime(time);
 my $datestr = ctime();
 print "$datestr\n";
 
-foreach my $file (@list) {
-	print "Copying: $file.js\n";
-	open(IN,"<$src/$file.js") or die $!;
+foreach my $file (@all_files) {
 	open(OUT,">$dest/$file.js") or die $!;
-	while(<IN>) {
-		s/(\@version).*/$1 $datestr/;
-		print OUT $_;
+	if(grep /$file/,@from_template) {
+		foreach my $temp_line (@template_lines) {
+			$temp_line =~ s/(\@version).*/$1 $datestr/;
+			print OUT $temp_line;
+		}
+	} else {
+		open(IN,"<$src/$file.js") or die $!;
+		while(<IN>) {
+			s/(\@version).*/$1 $datestr/;
+			print OUT $_;
+		}
 	}
+	close(OUT);
+	print "Copying: $file.js\n";
 }
 
 chdir($dest);
