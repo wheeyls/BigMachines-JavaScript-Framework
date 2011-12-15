799c9ff079dadbf06932d917e274e5ccf17d58fd
mwheeler <mwheeler@bigmachines.com>
2011-12-15 14:59:51 -0800

    Release rolled

--- js_starter/javascript/sitewide.js
+++ js_starter/javascript/sitewide.js
@@ -1,8 +1,10 @@
-/*global require */
-// Page Data Template 
+/**
+ * @version Mon Dec  5 09:51:20 2011
+ **/
 require([], function() {
 	/*
-	* Put all functions for commerce here
+	* Put all functions and objects here.
+	* Do not create global variables!
 	*/
 
 	//this function runs when the page loads
